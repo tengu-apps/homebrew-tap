0869a68d05f943bd4d6552f9277a94cfe4e13254
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-04-05 17:23:24 +0000

    Update tengu-init to 0.3.4

--- Formula/tengu-init.rb
+++ Formula/tengu-init.rb
@@ -1,38 +1,31 @@
 class TenguInit < Formula
   desc "Provision Tengu PaaS servers via SSH"
   homepage "https://github.com/tengu-apps/tengu-init"
-  version "0.3.3"
+  version "0.3.4"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/tengu-apps/tengu-init/releases/download/v#{version}/tengu-init-apple-silicon"
-      sha256 "dc16ae6a2e88420df0edababe814ab1ba7f158778adfe901e53e626880a576d4"
+      sha256 "663ebd8e44f33eae4ff718e03a5903f2261ea82bf17201115b6d1226b6f6469d"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/tengu-apps/tengu-init/releases/download/v#{version}/tengu-init-linux-arm64"
-      sha256 "6aa81436a2a75533329897ba4e71f17eec7aed0dd13c8fd273d05e4b665c97e4"
+      sha256 "afaf9372faf2e311397ad0f8a01db716e0c13bb6060f9d7bfad8577029b2cb35"
     end
+
     on_intel do
       url "https://github.com/tengu-apps/tengu-init/releases/download/v#{version}/tengu-init-linux-amd64"
-      sha256 "bbc5d2d8f7d6d8f2cca85efdfa5c2fec1fabaaf72dfdf6cde345edd25978e5d0"
+      sha256 "472128f633c053682771edd307360ee91e137dc889f274de1a51ee1146a3a15f"
     end
   end
 
   def install
-    binary_name = "tengu-init"
-    if OS.mac? && Hardware::CPU.arm?
-      binary_name = "tengu-init-apple-silicon"
-    elsif OS.linux? && Hardware::CPU.arm?
-      binary_name = "tengu-init-linux-arm64"
-    elsif OS.linux? && Hardware::CPU.intel?
-      binary_name = "tengu-init-linux-amd64"
-    end
-
-    bin.install binary_name => "tengu-init"
+    binary = Dir["tengu-init-*"].first || "tengu-init"
+    bin.install binary => "tengu-init"
   end
 
   test do
