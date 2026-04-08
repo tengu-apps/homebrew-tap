05626351b690eabae6fb50141a3d6688ed89cee2
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-04-08 14:47:24 +0000

    Update tengu-init to 0.5.0

--- Formula/tengu-init.rb
+++ Formula/tengu-init.rb
@@ -1,25 +1,25 @@
 class TenguInit < Formula
   desc "Provision Tengu PaaS servers via SSH"
   homepage "https://github.com/tengu-apps/tengu-init"
-  version "0.4.1"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/tengu-apps/tengu-init/releases/download/v#{version}/tengu-init-apple-silicon"
-      sha256 "38a1a6da4f442fb0423c30e1a4faa20341741597734c8cc0163828bf7524c038"
+      sha256 "32e6e6998c1f065b6d443d256798b1b5c5efca91f9a3a85172d892d35e13167d"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/tengu-apps/tengu-init/releases/download/v#{version}/tengu-init-linux-arm64"
-      sha256 "81e9ab961921c539110dcd2fd8c1a0c8074bf502d4592c3af0e94bb72698d7bc"
+      sha256 "004c21296ff368db5d2a9f9fee2a61ba6c5d0e65767da48654673f4e41679c0b"
     end
 
     on_intel do
       url "https://github.com/tengu-apps/tengu-init/releases/download/v#{version}/tengu-init-linux-amd64"
-      sha256 "78d01508e32048f0d9f8f95981e66b0ed85573d4dda730821a7f587c8b748720"
+      sha256 "dbe6ec42a56b4fd4b0992ad7ae5623eb1a4635136d421b70e2b6d5382207c718"
     end
   end
 
