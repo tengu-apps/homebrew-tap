40d895d9ac7cc37bb0a68d65b32fb8d41ab2ff13
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-04-12 16:32:42 +0200

    tengu 0.2.7

--- Formula/tengu.rb
+++ Formula/tengu.rb
@@ -1,25 +1,25 @@
 class Tengu < Formula
   desc "Self-hosted PaaS for deploying web applications via git push"
   homepage "https://tengu.to"
-  version "0.2.6"
+  version "0.2.7"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/tengu-apps/homebrew-tap/releases/download/tengu-v#{version}/tengu-macos-arm64"
-      sha256 "be8733b844a9f6d2347010c127c76ed4c23e5864b0cc7ab28821e0821ac7ff7b"
+      sha256 "9641f61f44b181ccf20b6a9c709deeed841a74b04aeea5c5e06ef3b7e72a5d69"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/tengu-apps/homebrew-tap/releases/download/tengu-v#{version}/tengu-linux-arm64"
-      sha256 "412b84604884c3985375efc538002df38557be53b27ba4e59435bc87f49e3f2d"
+      sha256 "45ad90ffeb91079714fb3868140639d62b5786e95bf2a99070250ab589799934"
     end
 
     on_intel do
       url "https://github.com/tengu-apps/homebrew-tap/releases/download/tengu-v#{version}/tengu-linux-amd64"
-      sha256 "fda9a7870e4d9133fc7e30b1b71f2e07887000f2f026c18f95094bfc07700d40"
+      sha256 "d58c908d3e83ca5164d2fea12dada442cae6e39ae51600eee2072849bf1a04ca"
     end
   end
 
