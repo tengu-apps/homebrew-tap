14303d44b8157c443da13901d06d5936b72eecfb
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-04-17 00:17:55 +0200

    tengu 0.2.12

--- Formula/tengu.rb
+++ Formula/tengu.rb
@@ -1,37 +1,23 @@
 class Tengu < Formula
   desc "Self-hosted PaaS for deploying web applications via git push"
   homepage "https://tengu.to"
-  version "0.2.11"
+  version "0.2.12"
   license "MIT"
-
-  on_macos do
-    on_arm do
-      url "https://github.com/tengu-apps/homebrew-tap/releases/download/tengu-v#{version}/tengu-macos-arm64"
-      sha256 "5d7ff03320057ced80eed277eebd6675648c278c1c412b912487b6cf97c4989d"
-    end
-
-    on_intel do
-      url "https://github.com/tengu-apps/homebrew-tap/releases/download/tengu-v#{version}/tengu-macos-intel"
-      sha256 "dea58dd29f530124a8df1cbb3e4e13467bddd364d2f7cca69b380794383f0d9f"
-    end
-  end
 
   on_linux do
     on_arm do
       url "https://github.com/tengu-apps/homebrew-tap/releases/download/tengu-v#{version}/tengu-linux-arm64"
-      sha256 "d1af259119eed2fa9cb90138bcd78f29630e16f6667cfcc4e8e58c60cee1e36b"
+      sha256 "acc042dfabd027d83ca975c90ba95f0e1e5ede9a27bafe30409ceeaa2b43ef3c"
     end
 
     on_intel do
       url "https://github.com/tengu-apps/homebrew-tap/releases/download/tengu-v#{version}/tengu-linux-amd64"
-      sha256 "863c5c77131235f21d772d6d52a066915c12eeba1797506104947ebea5654144"
+      sha256 "dc233d3ef8083d1b1b145bb49813b397b9bb369f2bbbcda04bcc2743836a3146"
     end
   end
 
   def install
-    binary_name = if OS.mac?
-      Hardware::CPU.arm? ? "tengu-macos-arm64" : "tengu-macos-intel"
-    elsif OS.linux?
+    binary_name = if OS.linux?
       Hardware::CPU.arm? ? "tengu-linux-arm64" : "tengu-linux-amd64"
     end
     bin.install binary_name => "tengu"
